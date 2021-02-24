c275e22ab7a1a3c9dedc591b6f6b035cde56d2c7
isabellagh <isabellagh88@gmail.com>
2021-02-23 23:44:15 -0500

    migrations and associations

---
+++ db/migrate/20210224040631_create_portfolios.rb
@@ -0,0 +1,12 @@
+class CreatePortfolios < ActiveRecord::Migration[6.1]
+  def change
+    create_table :portfolios do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :title 
+      t.string :image 
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
